6171a2c3ac30ed02225342b21e2491186b3d8cc3
Mohammad Amin <mohammadaminalaee315@gmail.com>
2024-05-14 09:23:36 +0330

    test

--- src/components/Routes/NotFound.js
+++ src/components/Routes/NotFound.js
@@ -3,7 +3,6 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className={'w-full h-screen'}>
-
             <img src="https://static-00.iconduck.com/assets.00/404-page-not-found-illustration-2048x998-yjzeuy4v.png" alt=""/>
         </div>
     );
